d1486285bdb83ef719c08c8cea021ede12e1ebc9
Geoff <ucbtgje@ucl.ac.uk>
2015-02-20 11:29:27 +0000

    change ''' to """ in memoize

--- aol_model/memoize.py
+++ aol_model/memoize.py
@@ -2,10 +2,10 @@ import collections
 import functools
 
 class memoized(object):
-   '''Decorator. Caches a function's return value each time it is called.
+   """Decorator. Caches a function's return value each time it is called.
    If called later with the same arguments, the cached value is returned
    (not reevaluated).
-   '''
+   """
    def __init__(self, func):
       self.func = func
       self.cache = {}
@@ -21,8 +21,8 @@ class memoized(object):
          self.cache[args] = value
          return value
    def __repr__(self):
-      '''Return the function's docstring.'''
+      """Return the function's docstring."""
       return self.func.__doc__
    def __get__(self, obj, objtype):
-      '''Support instance methods.'''
+      """Support instance methods."""
       return functools.partial(self.__call__, obj)
